3ff50dfe6231c8af46a61bc1256617bc033b632a
Francis Phan <phan5@pdx.edu>
2022-04-01 15:20:44 -0700

    Commit skeleton About and inital Projects sections

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,11 +1,32 @@
-
-import React, {useState, useEffect} from 'react'
-import {motion} from 'framer-motion'
+import React from 'react'
+import { motion } from 'framer-motion'
 import './About.scss'
 
 const About = () => {
+  const placeholderImg = 'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc='
+  const abouts = [
+    { title: 'Backend Developer', description: 'I am a backend developer with a passion for building API endpoints and external connections.', imageUrl: placeholderImg },
+    { title: 'Frontend Developer', description: 'I am a frontend developer with a passion for designing beautiful and functional web applications.', imageUrl: placeholderImg },
+    { title: 'React Developer', description: 'I am a React developer who has experience with React Native and NextJS.', imageUrl: placeholderImg },
+    { title: 'Web Automation', description: 'I have experience with automating web tasks via Selenium automation.', imageUrl: placeholderImg },
+    { title: 'MERN Stack', description: 'I have experience building MERN applications.', imageUrl: placeholderImg },
+  ]
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'></h2>
+
+      <div className='app_profiles'>
+        {abouts.map((about, index) => {
+          return (
+            <motion.div className='app_profile-card' key={about.title + index} whileInView={{ opacity: 1 }} whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'spring' }}>
+              <img src={about.imageUrl} alt={about.title} />
+              <h2 className='bold-text'>{about.title}</h2>
+              <p className='p-text'>{about.description}</p>
+            </motion.div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AiFillEye, AiFillGitHub } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 import './Projects.scss'
 
 const Projects = () => {
+  const placeholderImg = 'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc='
+
+  const [container, setContainer] = useState({ y: 0, opacity: 1 });
   return (
-    <div>Projects</div>
+    <>
+      <h2 className='head-text'>My Collection of Projects</h2>
+
+      <motion.div className='app_projects-portfolio' animate={container} transition={{ duration: 0.5, delayChildren: 0.5 }}>
+        <div className='app_projects-item app_flex'>
+          <div className='app_projects-img app_flex'>
+            <img src={placeholderImg} alt='placeholder' />
+            <motion.div className='app_project-hover' whileHover={{ opacity: [0, 1] }}>
+              <a href='placeholder' target='_blank' rel='noreferrer' />
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
+    </>
   )
 }
 
